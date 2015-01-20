45ab375bad77e4c431f0916a692d5cdf30ba8481
Maxim Chvertkin <maxx11@gmail.com>
2015-01-21 01:47:54 +0400

    var fixes

--- splitter.rb
+++ splitter.rb
@@ -6,7 +6,7 @@ require 'sax-machine'
 
 fname = ARGV[0]
 fname_parts = fname.split('.')
-points_per_file = ARGV[1] || 50
+points_per_file = ARGV[1].to_i || 50
 @dir = 'split'
 
 raise "File #{fname} not found" unless File.exists?(fname)
@@ -25,15 +25,24 @@ file.close
 
 split_coords = {}
 file_num = 1
+last_coord = nil
 
+title = xml.css('Document name').first
+name  = xml.css('Placemark name').first
 coords = xml.css('coordinates').first
+
 coords.content.strip.split(' ').compact.each_with_index do |coord,i|
-  (split_coords[file_num] ||=[]) << coord
+  arr = split_coords[file_num] ||= []
+  arr << last_coord if arr.empty? && last_coord
+  arr << coord
 
   file_num += 1 if (i+1) % points_per_file == 0
+  last_coord = coord
 end
 
 split_coords.each do |i,coord|
+  title.content = "#{title.content} #{i}"
+  name.content  = "#{name.content} #{i}"
   coords.content = coord.join(' ')
   write_split_file "#{fname_parts[0]}-#{i}.#{fname_parts[1]}", xml
 end
